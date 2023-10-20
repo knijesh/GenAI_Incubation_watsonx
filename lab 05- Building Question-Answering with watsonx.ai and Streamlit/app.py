e6be8a4fe2c320f9685ba39ae938a13cf660be15
Kanjinghat Nijesh <niju.victor@gmail.com>
2023-10-20 10:16:24 +0800

    Add files via upload

---
+++ lab 05- Building Question-Answering with watsonx.ai and Streamlit/app.py
@@ -0,0 +1,55 @@
+# Import environment loading library
+from dotenv import load_dotenv
+# Import IBMGen Library 
+from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
+from langchain.llms.base import LLM
+# Import lang Chain Interface object
+from langChainInterface import LangChainInterface
+# Import langchain prompt templates
+from langchain.prompts import PromptTemplate
+# Import system libraries
+import os
+# Import streamlit for the UI 
+import streamlit as st
+
+
+
+# Load environment vars
+load_dotenv()
+
+# Define credentials 
+api_key = os.getenv("API_KEY", None)
+ibm_cloud_url = os.getenv("IBM_CLOUD_URL", None)
+project_id = os.getenv("PROJECT_ID", None)
+if api_key is None or ibm_cloud_url is None or project_id is None:
+    print("Ensure you copied the .env file that you created earlier into the same directory as this notebook")
+else:
+    creds = {
+        "url": ibm_cloud_url,
+        "apikey": api_key 
+    }
+
+# Define generation parameters 
+params = {
+    GenParams.DECODING_METHOD: "sample",
+    GenParams.MIN_NEW_TOKENS: 30,
+    GenParams.MAX_NEW_TOKENS: 300,
+    GenParams.TEMPERATURE: 0.2,
+    # GenParams.TOP_K: 100,
+    # GenParams.TOP_P: 1,
+    GenParams.REPETITION_PENALTY: 1
+}
+
+# define LangChainInterface model
+llm = LangChainInterface(model='google/flan-ul2', credentials=creds, params=params, project_id=project_id)
+
+# Title for the app
+st.title('🤖 Our First Q&A Front End')
+# Prompt box 
+prompt = st.text_input('Enter your prompt here')
+# If a user hits enter
+if prompt: 
+    # Pass the prompt to the llm
+    response = llm(prompt)
+    # Write the output to the screen
+    st.write(response)
